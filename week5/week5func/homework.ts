f200d69bea1a88e98c3320451773f8c6dc477587
Bryce's laptop <bryce.menadue98@gmail.com>
2019-04-06 16:29:06 -0500

    blogappinstall
    Initial set up

--- week5/week5func/homework.ts
+++ week5/week5func/homework.ts
@@ -1,3 +1,16 @@
-const f = (a: number) => a > 2;
+const startNums = [-3, -2, -1, 0, 1, 2, 3];
+const positiveNums = startNums.filter(num => num > 0);
 
-console.log(f);
+console.log(positiveNums);
+
+const availableToppings = [
+    { name: "Sausage", cost: 0.50 },
+    { name: "Cheese", cost: 0.0 },
+    { name: "Mushrooms", cost: 1.00 },
+];
+
+availableToppings.map((topping) => { topping.checked = false; return topping; });
+
+const numSum = startNums.reduce((total, num) => total += num);
+
+console.log(numSum);

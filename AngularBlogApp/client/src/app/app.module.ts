0115489edce9ea99fe1c09d6c215d52f826ef10c
Bryce's laptop <bryce.menadue98@gmail.com>
2019-04-06 18:26:06 -0500

    implementing user login validation

--- AngularBlogApp/client/src/app/app.module.ts
+++ AngularBlogApp/client/src/app/app.module.ts
@@ -6,14 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { RootComponent } from './root/root.component';
 import { LoginComponent } from './login/login.component';
-// import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 
 import { ROUTING } from './app.routing';
 @NgModule({
   declarations: [
     RootComponent,
     LoginComponent,
-    // HomeComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- AngularBlogApp/client/src/app/login/login.component.ts
+++ AngularBlogApp/client/src/app/login/login.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
- 
+import { LoginService } from './login.service';
+
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  providers: [ LoginService ]
 })
 export class LoginComponent {
- 
+
   constructor() {
-       
+
   }
- 
+
 }
